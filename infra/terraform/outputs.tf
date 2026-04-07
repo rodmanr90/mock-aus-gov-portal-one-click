983e74e8b19c77e53173c774f755747a154adf4a
Rod <rodman1919@hotmail.com>
2026-04-07 21:29:26 +1000

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,15 +1,11 @@
 output "ec2_public_ip" {
-  description = "Public IPv4 address of the EC2 instance"
-  value       = aws_instance.app.public_ip
+  value = aws_instance.app_server.public_ip
 }
 
 output "ec2_public_dns" {
-  description = "Public DNS of the EC2 instance"
-  value       = aws_instance.app.public_dns
+  value = aws_instance.app_server.public_dns
 }
 
 output "ec2_username" {
-  description = "SSH username for this AMI"
-  value       = var.ec2_username
+  value = "ubuntu"
 }
-
