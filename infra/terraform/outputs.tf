95e47c546a59a8d142994751e4017849136a7f85
Rod <rodman1919@hotmail.com>
2026-04-08 12:58:28 +1000

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,11 +1,14 @@
 output "ec2_public_ip" {
-  value = aws_instance.app_server.public_ip
+  description = "The public IP address of the app server"
+  value       = aws_instance.app_server.public_ip
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.app_server.public_dns
+  description = "The public DNS of the app server"
+  value       = aws_instance.app_server.public_dns
 }
 
 output "ec2_username" {
-  value = "ubuntu"
+  description = "The default system user for the AMI"
+  value       = "ubuntu"
 }
